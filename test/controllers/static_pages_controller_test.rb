a23f3defff8ed08604a4e0359ad8d3dbcc91756e
Andrey I. Esaulov <esaulov.andrey@gmail.com>
2022-10-06 12:00:04 +0200

    got the tests working again

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,26 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Random Thoughts"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Random Thoughts"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Random Thoughts"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do 
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Random Thoughts"
+    assert_select "title", "About | #{@base_title}"
   end
 end
