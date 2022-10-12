d05630758abbf377cf868a9ab07eadd90b5c055d
Andrey I. Esaulov <esaulov.andrey@gmail.com>
2022-10-12 17:38:22 +0200

    flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select '#error_explanation ul li', "Password is too short (minimum is 6 characters)"
     assert_select '#error_explanation ul li', "Password confirmation doesn't match Password"
   end
+
+  test "sucessful form submission" do
+    get signup_path
+    assert_difference 'User.count' do 
+      post users_path, params: { user: { name: "Valid User",
+                                         email: "valid@example.com",
+                                         password: "asdffdsa",
+                                         password_confirmation: "asdffdsa" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select '.alert-success', 'User profile created successfully!'
+    assert flash[:success]
+    assert_not flash[:error]
+    assert_not flash.empty?
+  end
 end
